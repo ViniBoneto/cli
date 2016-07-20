515f2320fdc43a32c14be48bb173cef8ccb10b78
AlexisNo <alexis@henaut.net>
2016-07-20 19:42:36 -0300

    Improve "lager new" to automatically install lager and lager plugins

--- src/cli/new.js
+++ src/cli/new.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
+const spawn = require('child_process').spawn;
 
 /**
  * This module exports a function that enrich the interactive command line and return a promise
@@ -41,18 +42,18 @@ module.exports = (icli) => {
   function getChoices() {
     return {
       plugins: [{
-        value: 'iam',
-        name: icli.format.info('iam') + ' - manage IAM policies and roles',
+        value: '@lager/iam',
+        name: icli.format.info('@lager/iam') + ' - manage IAM policies and roles',
         short: 'iam',
         checked: true
       }, {
-        value: 'api-gateway',
-        name: icli.format.info('api-gateway') + ' - deploy swagger definitions in API gateway',
+        value: '@lager/api-gateway',
+        name: icli.format.info('@lager/api-gateway') + ' - deploy swagger definitions in API gateway',
         short: 'api-gateway',
         checked: true
       }, {
-        value: 'node-lambda',
-        name: icli.format.info('node-lambda') + ' - deploy nodejs Lambda in AWS and associate them to API endpoints',
+        value: '@lager/node-lambda',
+        name: icli.format.info('@lager/node-lambda') + ' - deploy nodejs Lambda in AWS and associate them to API endpoints',
         short: 'node-lambda',
         checked: true
       }]
@@ -67,21 +68,57 @@ module.exports = (icli) => {
   function executeCommand(parameters) {
     // If a name has been provided, we create the project directory
     // Otherwise, the project will ne created in the current directory
-    const configFilePath = parameters.projectName ? path.join(process.cwd(), parameters.projectName) : process.cwd();
-    fs.mkdirAsync(configFilePath)
-    .then(() => {
+    let configFilePath = process.cwd();
+    let p = Promise.resolve();
+    if (parameters.projectName) {
+      configFilePath += path.sep + parameters.projectName;
+      //npmPrefix = '--prefix ' + parameters.projectName;
+      p = fs.mkdirAsync(configFilePath);
+    }
+    p.then(() => {
+      console.log('\n  Creating Lager config file (' + icli.format.info('lager.json') + ')');
       const projectConfig = {
         name: parameters.projectName || 'A Lager project',
-        plugins: parameters.plugins.map((pluginName) => '@lager/' + pluginName)
+        plugins: parameters.plugins
       };
       return fs.writeFileAsync(configFilePath + path.sep + 'lager.json', JSON.stringify(projectConfig, null, 2));
     })
     .then(() => {
-      let msg = '\n  A new lager project has been created!\n\n';
+      console.log('\n  Creating a ' + icli.format.info('package.json') + ' file');
+      const packageConfig = {};
+      return fs.writeFileAsync(configFilePath + path.sep + 'package.json', JSON.stringify(packageConfig, null, 2));
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('\n  Installing Lager and Lager plugins');
+        const cmdArgs = parameters.plugins;
+        cmdArgs.unshift('@lager/lager');
+        cmdArgs.unshift('--save');
+        cmdArgs.unshift('install');
+        console.log('  Running ' + icli.format.cmd('npm ' + cmdArgs.join(' ') + '\n'));
+        const npmInstall = spawn('npm', cmdArgs, { cwd: parameters.projectName });
+        npmInstall.stdout.on('data', data => console.log(data.toString('utf8')));
+        npmInstall.stderr.on('data', data => console.error(data.toString('utf8')));
+        npmInstall.on('close', code => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject(new Error(' npm install exited with code ' + code));
+          }
+        });
+        npmInstall.on('error', e => {
+          console.error(icli.format.ko(e));
+          reject(e);
+        });
+      });
+    })
+    .then(() => {
+      let msg = icli.format.ok('  A new lager project has been created!\n\n');
       if (parameters.projectName) {
         msg += '  You should now enter in the ' + icli.format.info(parameters.projectName) + ' folder to start working\n';
       }
-      msg += '  Execute the ' + icli.format.cmd('lager -h') + ' command in the root folder of the project to see available commands\n';
+      msg += '  Execute ' + icli.format.cmd('lager -h') + ' in the root folder of the project to see available commands\n';
+      msg += '  Execute ' + icli.format.cmd('npm init') + ' to complete the configuration of the project\'s ' + icli.format.info('package.json') + ' file\n';
       console.log(msg);
     });
   }
